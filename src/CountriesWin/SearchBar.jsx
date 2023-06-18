b1996d3b3250d3dda840097b6fbc0ac60ac9765a
HarshDagar <harshdagar8181@gmail.com>
2023-06-18 13:54:39 +0530

    testing searchbar

--- src/CountriesWin/SearchBar.jsx
+++ src/CountriesWin/SearchBar.jsx
@@ -29,9 +29,9 @@ const SearchBar = ({searchByName,searchByRegion}) => {
             <div className="searchbar">
                 <div id="searcharea">
 
-                    <label htmlFor="username"><i style={{color: "rgba(28, 107, 255, 0.81)"}}
+                    <label htmlFor="countryname"><i style={{color: "rgba(28, 107, 255, 0.81)"}}
                             className="fa fa-sharp fa-light fa-search"></i></label>
-                    <input onChange={handleChange} type="text" name="username" id="username" value={formData} placeholder="Search for a country..."/>
+                    <input onChange={handleChange} type="text" name="countryname" id="countryname" value={formData} placeholder="Search for a country..."/>
                 </div>
                 <button type="submit">Search</button>
             </div>
